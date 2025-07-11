a081d723fdf02182c3158ed0d47a4935418769ba
mainishiumi-ivis <mai.nishiumi@ivis.co.jp>
2025-07-11 10:03:22 +0900

    Revert "Topページ遷移時URLにidを残さないように修正"
    This reverts commit e7171298bd63273461f2192c2ce5191dcfe4bdc8.

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -162,7 +162,7 @@ function Header() {
               id: 'app.header.nav.top',
               defaultMessage: 'Top',
             })}
-            asLink={<RouterLink to={urls.national[lang]} />}
+            asLink={<RouterLink to={urls.national[lang] + search} />}
           />
           <NavItem
             current={
